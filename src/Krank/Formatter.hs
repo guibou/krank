fb42c743ebcb8993bc44f25a92c0f8d76739c806
Grégory Bataille <gregory.bataille@gmail.com>
2019-09-15 12:00:57 +0200

    feat: simple formatter for single file logic

--- src/Krank/Formatter.hs
+++ src/Krank/Formatter.hs
@@ -1,9 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Krank.Formatter (
   showViolations
   ) where
 
+import Data.Text (Text, append, pack)
+
 import Krank.Types
 
 showViolations :: [Violation]
-               -> String
-showViolations violations = show violations
+               -> Text
+showViolations = foldl (\acc v -> acc `append` showViolation v) ""
+
+showViolation :: Violation
+              -> Text
+showViolation violation = "\n"
+  `append` "[" `append` pack (show (level violation)) `append` "]" `append` " " `append` message violation `append` "\n"
+  `append` "    in: " `append` snippet violation
