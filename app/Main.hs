2d64f23e5aa477871920ffbceeddbf38bbf98f10
Grégory Bataille <gregory.bataille@gmail.com>
2019-09-15 11:36:21 +0200

    feat: skeleton - get a file from the CLI args and run krank against it

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,24 @@
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+
+import Krank
+
 main :: IO ()
 main = do
-  putStrLn "krank is cool"
+  filePath <- parseArgs
+  case filePath of Just f -> putStrLn . show $ processFile f
+                   Nothing -> printHelp >> exitFailure
+
+parseArgs :: IO (Maybe FilePath)
+parseArgs = do
+  args <- getArgs
+  case args of [a] -> pure $ Just a
+               _   -> pure $ Nothing
+
+printHelp :: IO ()
+printHelp = do
+  putStrLn "Usage: krank <filename>"
+  putStrLn ""
+  putStrLn "  filename: path to the file to check"
+  putStrLn ""
+  putStrLn "Example: krank app/Main.hs"
