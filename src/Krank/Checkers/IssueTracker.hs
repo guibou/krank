79eabac7f1f6b40a45cbe708b6998f82ffdf4d0a
Grégory Bataille <gregory.bataille@gmail.com>
2019-09-22 15:05:45 +0200

    feat: github issue RE

---
+++ src/Krank/Checkers/IssueTracker.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Krank.Checkers.IssueTracker (
+  check
+  , githubRE
+  ) where
+
+import Control.Applicative ((*>), optional)
+import Data.Char (isDigit)
+import Text.Regex.Applicative ((=~), RE(), anySym, few, psym)
+
+import Krank.Types
+
+githubRE :: RE Char Int
+githubRE = do
+  optional ("http" *> optional "s" *> "://")
+  optional "www."
+  "github.com/"
+  few anySym
+  "/"
+  few anySym
+  "/"
+  issueNum <- few (psym isDigit)
+  optional "/"
+  return $ read issueNum
+
+check :: FilePath
+      -> [Violation]
+check file = []
