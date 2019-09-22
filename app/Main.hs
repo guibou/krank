ae6f461701e4e3f686fb5707c8d4fa3aaa1a60c4
Grégory Bataille <gregory.bataille@gmail.com>
2019-09-22 11:52:56 +0200

    feat: introduce optparse-applicative + add --help

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,44 @@
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 
+import Data.Semigroup ((<>))
 import Data.Text (unpack)
+import Options.Applicative (
+  (<**>), Parser(), ParserInfo(), execParser, fullDesc, header, helper, info, progDesc
+                           )
 
 import Krank
 import Krank.Formatter
 
 main :: IO ()
 main = do
-  filePath <- parseArgs
-  case filePath of Just f -> putStrLn . unpack . showViolations $ processFile f
-                   Nothing -> printHelp >> exitFailure
+  options <- execParser opts
+  putStrLn "Program launched"
+--   filePath <- parseArgs
+--   case filePath of Just f -> putStrLn . unpack . showViolations $ processFile f
+--                    Nothing -> printHelp >> exitFailure
+--
+-- parseArgs :: IO (Maybe FilePath)
+-- parseArgs = do
+--   args <- getArgs
+--   case args of [a] -> pure $ Just a
+--                _   -> pure Nothing
+--
+-- printHelp :: IO ()
+-- printHelp = do
+--   putStrLn "Usage: krank <filename>"
+--   putStrLn ""
+--   putStrLn "  filename: path to the file to check"
+--   putStrLn ""
+--   putStrLn "Example: krank app/Main.hs"
 
-parseArgs :: IO (Maybe FilePath)
-parseArgs = do
-  args <- getArgs
-  case args of [a] -> pure $ Just a
-               _   -> pure Nothing
+data Sample = Sample
 
-printHelp :: IO ()
-printHelp = do
-  putStrLn "Usage: krank <filename>"
-  putStrLn ""
-  putStrLn "  filename: path to the file to check"
-  putStrLn ""
-  putStrLn "Example: krank app/Main.hs"
+sample :: Parser Sample
+sample = pure Sample
+
+opts :: ParserInfo Sample
+opts = info (sample <**> helper)
+  ( fullDesc
+  <> progDesc "Checks the comments of a code file"
+  <> header "krank - a comment linter / analytics tool" )
