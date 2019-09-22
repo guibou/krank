2ebbf72d3e13b62fba84692416928516c7cf0b42
Grégory Bataille <gregory.bataille@gmail.com>
2019-09-22 12:04:03 +0200

    feat: expect one argument to the program

--- app/Main.hs
+++ app/Main.hs
@@ -3,42 +3,30 @@ import System.Exit (exitFailure)
 
 import Data.Semigroup ((<>))
 import Data.Text (unpack)
-import Options.Applicative (
-  (<**>), Parser(), ParserInfo(), execParser, fullDesc, header, helper, info, progDesc
-                           )
+import qualified Options.Applicative as Opt
+import Options.Applicative ((<**>))
 
 import Krank
 import Krank.Formatter
 
+data Sample = Sample {
+  codeFilePath :: FilePath
+}
+
+fileToParse :: Opt.Parser FilePath
+fileToParse = Opt.argument Opt.str (Opt.metavar "FILE")
+
+sample :: Opt.Parser Sample
+sample = Sample
+  <$> fileToParse
+
+opts :: Opt.ParserInfo Sample
+opts = Opt.info (sample <**> Opt.helper)
+  ( Opt.fullDesc
+  <> Opt.progDesc "Checks the comments in FILE"
+  <> Opt.header "krank - a comment linter / analytics tool" )
+
 main :: IO ()
 main = do
-  options <- execParser opts
-  putStrLn "Program launched"
---   filePath <- parseArgs
---   case filePath of Just f -> putStrLn . unpack . showViolations $ processFile f
---                    Nothing -> printHelp >> exitFailure
---
--- parseArgs :: IO (Maybe FilePath)
--- parseArgs = do
---   args <- getArgs
---   case args of [a] -> pure $ Just a
---                _   -> pure Nothing
---
--- printHelp :: IO ()
--- printHelp = do
---   putStrLn "Usage: krank <filename>"
---   putStrLn ""
---   putStrLn "  filename: path to the file to check"
---   putStrLn ""
---   putStrLn "Example: krank app/Main.hs"
-
-data Sample = Sample
-
-sample :: Parser Sample
-sample = pure Sample
-
-opts :: ParserInfo Sample
-opts = info (sample <**> helper)
-  ( fullDesc
-  <> progDesc "Checks the comments of a code file"
-  <> header "krank - a comment linter / analytics tool" )
+  options <- Opt.execParser opts
+  putStrLn . unpack . showViolations . processFile . codeFilePath $ options
