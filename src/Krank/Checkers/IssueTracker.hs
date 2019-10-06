a25d24b7eb8841abcec587fd26c42c4b3507104f
Grégory Bataille <gregory.bataille@gmail.com>
2019-10-06 10:27:21 +0200

    feat: get status for github issues

--- src/Krank/Checkers/IssueTracker.hs
+++ src/Krank/Checkers/IssueTracker.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Krank.Checkers.IssueTracker (
@@ -8,35 +9,58 @@ module Krank.Checkers.IssueTracker (
   , githubRE
   , gitlabRE
   , gitRepoRE
+
   ) where
 
 import Control.Applicative ((*>), optional)
+import Data.Aeson (Value, (.:))
+import qualified Data.Aeson.Types as AesonT
 import Data.Char (isDigit)
 import Data.Maybe (fromMaybe)
+import Data.Text (Text, pack)
+import qualified Network.HTTP.Req as Req
 import System.IO (readFile)
-import Text.Regex.Applicative ((=~), RE(), anySym, few, many, psym, some, string)
+import Text.Regex.Applicative ((=~), RE(), few, psym, some, string)
 
 import Krank.Checkers.Common
-import Krank.Types
+
+data GitServer = Github | Gitlab deriving (Eq, Show)
+
+data IssueStatus = Open | Closed deriving (Eq, Show)
+-- TODO - can it be done with readPrec?
+instance Read IssueStatus where
+  readsPrec _ "closed" = [(Closed, "")]
+  readsPrec _ _ = [(Open, "")]
 
 data GitIssue = GitIssue {
-  owner :: String,
-  repo :: String,
+  server :: GitServer,
+  owner :: Text,
+  repo :: Text,
   issueNum :: Int
 } deriving (Eq, Show)
 
+data GitIssueWithStatus = GitIssueWithStatus {
+  gitIssue :: GitIssue,
+  issueStatus :: IssueStatus
+} deriving (Eq, Show)
+
+serverDomain :: GitServer
+             -> String
+serverDomain Github = "github.com"
+serverDomain Gitlab = "gitlab.com"
+
 githubRE :: RE Char GitIssue
-githubRE = gitRepoRE "github.com"
+githubRE = gitRepoRE Github
 
 gitlabRE :: RE Char GitIssue
-gitlabRE = gitRepoRE "gitlab.com"
+gitlabRE = gitRepoRE Gitlab
 
-gitRepoRE :: String
+gitRepoRE :: GitServer
           -> RE Char GitIssue
-gitRepoRE host = do
+gitRepoRE gitServer = do
   optional ("http" *> optional "s" *> "://")
   optional "www."
-  string host
+  string (serverDomain gitServer)
   "/"
   repoOwner <- few (psym ('/' /=))
   "/"
@@ -45,7 +69,7 @@ gitRepoRE host = do
   "issues/"
   issueNumStr <- some (psym isDigit)
   -- Note that read is safe because of the regex parsing
-  return $ GitIssue repoOwner repoName (read issueNumStr)
+  return $ GitIssue gitServer (pack repoOwner) (pack repoName) (read issueNumStr)
 
 extractIssues :: String
               -> [GitIssue]
@@ -56,8 +80,37 @@ extractIssues toCheck =
       mMatches = (=~) toCheck . multiple <$> patterns
       matches = fromMaybe [] <$> mMatches
 
+-- Supports only github for the moment
+issueUrl :: GitIssue
+         -> Req.Url 'Req.Https
+issueUrl issue
+  | server issue == Github = Req.https "api.github.com" Req./: "repos" Req./: owner issue Req./: repo issue Req./: "issues" Req./: (pack . show $ issueNum issue)
+  | server issue == Gitlab = Req.https "google.com"
+
+restIssue :: Req.Url 'Req.Https
+            -> IO Value
+restIssue url = Req.runReq Req.defaultHttpConfig $ do
+  r <- Req.req Req.GET url Req.NoReqBody Req.jsonResponse (Req.header "User-Agent" "krank")
+  pure $ Req.responseBody r
+
+getStatus :: Value
+          -> AesonT.Result String
+getStatus (AesonT.Object o) = AesonT.parse (\x -> x .: "state") o
+getStatus _ = AesonT.Error "invalid JSON"
+
+parseStatus :: AesonT.Result String
+            -> Either String IssueStatus
+parseStatus (AesonT.Success status) = Right $ read status
+parseStatus (AesonT.Error err) = Left err
+
 check :: FilePath
-      -> IO [GitIssue]
+      -> IO [Either String GitIssueWithStatus]
 check file = do
   content <- readFile file
-  pure $ extractIssues content
+  let issues = extractIssues content
+  let urls = issueUrl <$> issues
+  statuses <- mapM restIssue urls
+  pure $ zipWith f issues (fmap (parseStatus . getStatus) statuses)
+    where
+      f _ (Left err) = Left err
+      f issue (Right is) = Right $ GitIssueWithStatus issue is
