3276ecfd3273cdf15de25e3d20e800edc34284dd
Grégory Bataille <gregory.bataille@gmail.com>
2019-10-17 11:22:53 +0200

    feat: github-key as commandline param

--- app/Main.hs
+++ app/Main.hs
@@ -3,29 +3,37 @@
 
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
-
 import System.IO (hPutStrLn, stderr)
 
+import Control.Exception.Safe
+import Control.Applicative (optional)
 import Data.Semigroup ((<>))
 import Data.Text (unpack)
 import qualified Options.Applicative as Opt
 import Options.Applicative ((<**>), many)
-
-import Control.Exception.Safe
 import PyF
 
 import Krank
 import Krank.Formatter
 
 data KrankOpts = KrankOpts {
-  codeFilePaths :: [FilePath]
+  codeFilePaths :: [FilePath],
+  githubKey :: Maybe String
 }
 
 filesToParse :: Opt.Parser [FilePath]
 filesToParse = many (Opt.argument Opt.str (Opt.metavar "FILES..."))
 
+githubKeyToParse :: Opt.Parser (Maybe String)
+githubKeyToParse = optional $ Opt.strOption $
+  Opt.long "github-key"
+  <> Opt.metavar "DEVELOPER_KEY"
+  <> Opt.help "A github developer key to allow for more API calls"
+
 sample :: Opt.Parser KrankOpts
-sample = KrankOpts <$> filesToParse
+sample = KrankOpts
+  <$> filesToParse
+  <*> githubKeyToParse
 
 opts :: Opt.ParserInfo KrankOpts
 opts = Opt.info (sample <**> Opt.helper)
@@ -36,6 +44,7 @@ opts = Opt.info (sample <**> Opt.helper)
 main :: IO ()
 main = do
   options <- Opt.execParser opts
+  putStrLn $ show (githubKey options)
   (flip mapM_) (codeFilePaths options) $ \path -> do
     (processFile path >>= putStrLn . unpack . showViolations)
     `catchAnyDeep` (\(SomeException e) -> hPutStrLn stderr [fmt|Error when processing {path}: {show e}|])
