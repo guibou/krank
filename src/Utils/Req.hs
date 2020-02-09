91dea2b0dc6a3978f93d3354da57239c18e05b08
Grégory Bataille <gregory.bataille@gmail.com>
2020-02-09 17:38:18 +0100

    feat: handle StatusCodeException

--- src/Utils/Req.hs
+++ src/Utils/Req.hs
@@ -1,12 +1,17 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Utils.Req (
   showHTTPException
   ) where
 
+import Data.ByteString.Char8 (ByteString)
 import Data.Text (Text, pack)
 import qualified Network.HTTP.Client as Client
 import qualified Network.HTTP.Req as Req
+import Network.HTTP.Types (Status(..))
+
+import PyF (fmt)
 
 showHTTPException :: Req.HttpException
                   -> Text
@@ -20,5 +25,16 @@ showClientHttpException (Client.InvalidUrlException _ reason) = pack reason
 
 showExceptionContent :: Client.HttpExceptionContent
                      -> Text
+showExceptionContent (Client.StatusCodeException resp body) = showResponse resp body
 -- Catch all doing a simple and ugly show
 showExceptionContent exc = pack . show $ exc
+
+showResponse :: Client.Response ()
+             -> ByteString
+             -> Text
+showResponse resp body = [fmt|\
+HTTP call failed: {show status} - {show statusMsg}
+      {show body}\
+|]
+  where status = statusCode . Client.responseStatus $ resp
+        statusMsg = statusMessage . Client.responseStatus $ resp
