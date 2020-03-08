4939914292b7e460b5c5d3321d16ae9942119cad
Grégory Bataille <gregory.bataille@retinai.com>
2020-03-08 11:04:34 +0100

    refactor: better error message display

---
+++ src/Utils/Display.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Utils.Display
+  ( indent,
+  )
+where
+
+import qualified Data.Text as Text
+import PyF (fmt)
+
+-- | indent the text given by a certain number of space character
+-- If the text given contains multiple lines, all the lines but the first will be prefixed by the
+-- continuation character '|'
+indent :: Int -> Text.Text -> Text.Text
+indent nbSpaces text = indentedText
+  where
+    (firstLine : nextLines) = Text.splitOn "\n" text
+    prefixedLines = map (\a -> [fmt|| {a}|]) nextLines
+    indentedLines = map (\a -> [fmt|{take nbSpaces $ repeat ' '}{a}|]) (firstLine : prefixedLines)
+    indentedText = Text.intercalate "\n" indentedLines
