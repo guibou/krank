4112a80b814d31b92c261e2babd378af05a0f215
Grégory Bataille <gregory.bataille@gmail.com>
2020-03-22 08:43:25 +0100

    feat: api rate limit and private repo errors for gitlab

--- src/Utils/Gitlab.hs
+++ src/Utils/Gitlab.hs
@@ -1,13 +1,93 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Utils.Gitlab
   ( showGitlabException,
+    gitlabAPILimitErrorText,
+    gitlabNotFoundErrorText,
   )
 where
 
-import Data.Text (Text)
+import Data.Aeson (FromJSON, ToJSON, decode)
+import Data.ByteString.Char8 (ByteString)
+import Data.ByteString.Lazy (fromStrict)
+import Data.Text (Text, isPrefixOf)
+import GHC.Generics (Generic)
+import qualified Network.HTTP.Client as Client
 import qualified Network.HTTP.Req as Req
+import Network.HTTP.Types (Status (..))
+import PyF (fmt)
 import Utils.Req (showHTTPException, showRawResponse)
 
+{- HLint ignore "Use newtype instead of data" -}
+
+-- | Represents a typical Gitlab Error serialized as JSON like so:
+--
+-- @
+-- {
+--    "message": "the error reason"
+-- }
+-- @
+data GitlabError
+  = GitlabError
+      { message :: Text
+      }
+  deriving (Generic, Show, FromJSON, ToJSON)
+
+-- | Uses the helper to show generic HTTP issues and provides a specific handler for Gitlab
+-- "business" exceptions
 showGitlabException ::
   Req.HttpException ->
   Text
-showGitlabException = showHTTPException showRawResponse
+showGitlabException = showHTTPException handleGitlabException
+
+handleGitlabException ::
+  Client.Response () ->
+  ByteString ->
+  Text
+handleGitlabException resp body =
+  case statusCode (Client.responseStatus resp) of
+    400 -> tryShowNiceErr
+    404 -> gitlabNotFoundErrorText
+    403 -> tryShowNiceErr
+    _ -> showRawResponse resp body
+  where
+    err = decode $ fromStrict body :: Maybe GitlabError
+    tryShowNiceErr = case err of
+      Just gitlabError -> showRawGitlabMessage gitlabError
+      Nothing -> showRawResponse resp body
+
+showRawGitlabMessage ::
+  GitlabError ->
+  Text
+showRawGitlabMessage err
+  | isApiRateLimitError msg = gitlabAPILimitErrorText
+  | otherwise = [fmt|From Gitlab: {msg}|]
+  where
+    msg = message err
+
+gitlabNotFoundErrorText :: Text
+gitlabNotFoundErrorText =
+  [fmt|\
+Could not find the indicated url.
+It's possible that you have mistyped the URL
+If not, this URL likely points to a private repository and you need to be authenticated to query its issues.
+You might want to provide a gitlab API key with the --issuetracker-gitlabhost option.
+See https://github.com/guibou/krank/blob/master/docs/Checkers/IssueTracker.md#gitlab|]
+
+gitlabAPILimitErrorText :: Text
+gitlabAPILimitErrorText =
+  [fmt|\
+Gitlab API Rate limit exceeded.
+You might want to provide a gitlab API key with the --issuetracker-gitlabhost option.
+See https://github.com/guibou/krank/blob/master/docs/Checkers/IssueTracker.md#gitlab|]
+
+apiRateLimitPrefix :: Text
+apiRateLimitPrefix = "API rate limit exceeded"
+
+isApiRateLimitError ::
+  Text ->
+  Bool
+isApiRateLimitError = isPrefixOf apiRateLimitPrefix
---
+++ tests/Test/Utils/GitlabSpec.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- Leaving unnecessary do because
+--  * they improve readability
+--  * if you add a context, or an it, it'll still compile, without remembering that there was no do
+--  before because at the time there was a single function inside
+--  * without those do, ormolu does some formatting that looks crazy to me
+{- Hlint ignore "Redundant do" -}
+
+module Test.Utils.GitlabSpec
+  ( spec,
+  )
+where
+
+import Data.Aeson (Value (..), encode)
+import Data.ByteString.Lazy (ByteString, toStrict)
+import Data.HashMap.Strict (singleton)
+import Data.Text (Text, isInfixOf, isPrefixOf)
+import Network.HTTP.Client (HttpException (..), HttpExceptionContent (..), createCookieJar, defaultRequest)
+import Network.HTTP.Client.Internal (Response (..), ResponseClose (..))
+import Network.HTTP.Req (HttpException (..))
+import Network.HTTP.Types (Status, status400, status403, status404)
+import Network.HTTP.Types.Version (http11)
+import Test.Hspec
+import Utils.Gitlab (gitlabAPILimitErrorText, gitlabNotFoundErrorText, showGitlabException)
+import Utils.Req (showHTTPException, showRawResponse)
+
+spec :: Spec
+spec = do
+  describe "Utils.Gitlab" $ do
+    context "Gitlab \"business\" errors, 4xx status code, \"message\" structure" $ do
+      it "Display the content of the error message, deserialized from the JSON" $ do
+        let errorMsg = "some_error"
+        let body = mkGitlabErrorBody errorMsg
+        let errorText = getErrorText body status400
+        errorText `shouldSatisfy` isPrefixOf "From Gitlab:"
+        errorText `shouldSatisfy` isInfixOf errorMsg
+    context "Gitlab API Rate Limit error" $ do
+      it "Displays the specific message for API errors" $ do
+        let errorMsg = "API rate limit exceeded for 86.111.137.132. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)"
+        let body = mkGitlabErrorBody errorMsg
+        let errorText = getErrorText body status403
+        errorText `shouldSatisfy` isInfixOf gitlabAPILimitErrorText
+    context "Gitlab Private repo error" $ do
+      it "Displays the specific message for private repo error errors" $ do
+        let errorMsg = "Not Found"
+        let body = mkGitlabErrorBody errorMsg
+        let errorText = getErrorText body status404
+        errorText `shouldSatisfy` isInfixOf gitlabNotFoundErrorText
+    context "Non-JSON response exception" $ do
+      it "Shows the raw exception, through the generic helper" $ do
+        let exception = JsonHttpException "JSON decoding failed"
+        showGitlabException exception `shouldBe` showHTTPException showRawResponse exception
+    context "Invalid URL exception" $ do
+      it "Shows the raw exception, through the generic helper" $ do
+        let exception = VanillaHttpException $ InvalidUrlException "file://foo" "bar"
+        showGitlabException exception `shouldBe` showHTTPException showRawResponse exception
+    context "otherHttpException" $ do
+      it "Shows the raw exception, through the generic helper" $ do
+        let exception = VanillaHttpException $ HttpExceptionRequest defaultRequest ConnectionTimeout
+        showGitlabException exception `shouldBe` showHTTPException showRawResponse exception
+
+dummyResponse :: Status -> Response ()
+dummyResponse status = Response status http11 [] () (createCookieJar []) (ResponseClose $ pure ())
+
+-- | From a raw error message to a JSON formatted gitlab error
+mkGitlabErrorBody :: Text -> ByteString
+mkGitlabErrorBody msg = encode $ Object $ singleton "message" (String msg)
+
+-- | From a raw JSON response body and status code to the interpreted displayed text
+getErrorText :: ByteString -> Status -> Text
+getErrorText body status = showGitlabException $ VanillaHttpException $ HttpExceptionRequest defaultRequest (StatusCodeException (dummyResponse status) (toStrict body))
